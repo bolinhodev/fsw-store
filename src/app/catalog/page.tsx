76f6c78a6f4486235b05491bce507617fab8704d
bolinhodev <129420520+bolinhodev@users.noreply.github.com>
2023-10-20 17:38:13 -0300

    fix: fixed the pages

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -8,7 +8,10 @@ const CatalogPage = async () => {
 
   return (
     <div className="flex flex-col gap-8 p-5">
-      <Badge className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase" variant="outline">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
         <ShapesIcon size={16} />
         Catálogo
       </Badge>
